cbb8d488eb3bbf0f5baae3c228573d8bb68869cf
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-05-19 20:11:07 +0200

    feat: migration info

--- database/migrations/2023_05_19_174549_create_projects_table.php
+++ database/migrations/2023_05_19_174549_create_projects_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100)->unique();
+            $table->string('language', 50);
+            $table->date('publication_date');
+            $table->string('link');
+            $table->string('slug', 100);
             $table->timestamps();
         });
     }
